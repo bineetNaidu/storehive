b69017cf607670997b573f4f7963e56669bd6649
Bineet Naidu <bineet.s.naidu@gmail.com>
2023-04-07 18:45:19 +0530

    🥡fix styles in `Navbar.tsx`

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -9,8 +9,8 @@ type ButtonProps = {
 
 const Button: FC<ButtonProps> = ({ tittle, icon }) => {
   return (
-    <button className="flex px-3 py-1 transition-all hover:underline">
-      <span>{!!icon && icon}</span>
+    <button className="flex items-center justify-center text-gray-700 hover:text-orange-500 transition-all text-sm font-semibold">
+      {!!icon && <span>{icon}</span>}
       <span className="ml-2">{tittle}</span>
     </button>
   );
@@ -18,7 +18,7 @@ const Button: FC<ButtonProps> = ({ tittle, icon }) => {
 
 export const Navbar: FC = () => {
   return (
-    <nav className="flex justify-between mx-auto px-5">
+    <nav className="flex justify-between mx-auto px-8 py-4">
       <div className="">
         <Link href="/">
           <h1 className="text-2xl font-bold text-orange-500">StoreHive</h1>
@@ -28,10 +28,12 @@ export const Navbar: FC = () => {
       <div className="flex justify-evenly w-full">
         <Button tittle="Categories" />
         <Button tittle="Deals" />
-        <Button tittle="What" />
+        <Button tittle="What's New" />
         <input
+          placeholder="Search"
           type="text"
-          className="border border-transparent hover:border-orange-950 rounded px-3 py-1 transition-all"
+          className="border border-orange-400 border-transparent hover:border-orange-700 rounded-xl px-3 py-1 transition-all focus:outline-none focus:ring-2 focus:ring-orange-500 focus:border-transparent focus:ring-opacity-50 focus:ring-offset-2 focus:ring-offset-orange-200 focus:ring-offset-opacity-50
+					"
         />
         <Button tittle="My Account" icon={<User />} />
         <Button tittle="Cart" icon={<ShoppingCart />} />
