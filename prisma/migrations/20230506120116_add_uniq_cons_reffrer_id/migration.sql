275e116e8eb0574fd13bfea99385fb7cb412547c
Bineet Naidu <bineet.s.naidu@gmail.com>
2023-06-02 23:33:23 +0530

    👎switched to app router & update docker-compose

--- prisma/migrations/20230506120116_add_uniq_cons_reffrer_id/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[referrer_id]` on the table `User` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "User_referrer_id_key" ON "User"("referrer_id");
--- src/pages/register.tsx
+++
@@ -1,8 +0,0 @@
-import { SignUp } from '@clerk/nextjs';
-import type { NextPage } from 'next';
-
-const RegisterPage: NextPage = () => (
-  <SignUp path="/register" routing="path" signInUrl="/login" />
-);
-
-export default RegisterPage;
