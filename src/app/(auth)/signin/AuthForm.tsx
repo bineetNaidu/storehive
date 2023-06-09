96da497cbf806fb49d5502cf7cd3feafbdcc179e
Bineet Naidu <bineet.s.naidu@gmail.com>
2023-06-09 11:40:02 +0530

    🚀 add signin handler w/ minor UI updates

--- src/app/(auth)/signin/AuthForm.tsx
+++ src/app/(auth)/signin/AuthForm.tsx
@@ -4,24 +4,38 @@ import { FC, useState } from 'react';
 import { Button } from '@/components/Button';
 import { InputField } from '@/components/InputField';
 import { FaGoogle, FaGithub, FaTwitter } from 'react-icons/fa';
+import { toast } from 'react-toastify';
+import { signIn } from 'next-auth/react';
+import Link from 'next/link';
 
 export const AuthForm: FC = () => {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
   const [rememberMe, setRememberMe] = useState(false);
 
-  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault();
     // Handle form submission logic here
+    try {
+      if (!email || !password) {
+        toast.error('Please fill all the fields');
+        return;
+      }
+
+      await signIn('credentials', {
+        email,
+        password,
+        callbackUrl: '/',
+      });
+    } catch (error: any) {
+      alert(error.message);
+    }
   };
 
   return (
     <div className="flex justify-center items-center">
-      <div className="w-full max-w-sm">
-        <form
-          onSubmit={handleSubmit}
-          className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4"
-        >
+      <div className="w-full max-w-sm bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+        <form onSubmit={handleSubmit}>
           <div className="mb-6">
             <InputField
               label="Your Email?"
@@ -55,27 +69,25 @@ export const AuthForm: FC = () => {
           </div>
           <div className="flex items-center justify-between">
             <Button type="submit">Sign In</Button>
-            <Button size="sm" varient="secondary">
-              Forgot Password?
-            </Button>
-          </div>
-          <div className="divider">OR</div>
-          <div className="flex justify-center gap-2">
-            <Button varient="outlined">
-              <FaGoogle />
-            </Button>
-            <Button varient="outlined">
-              <FaTwitter />
-            </Button>
-            <Button varient="outlined">
-              <FaGithub />
-            </Button>
+            <Link href="/register">
+              <Button size="sm" varient="secondary">
+                Create an account?
+              </Button>
+            </Link>
           </div>
         </form>
-        <p className="text-center text-gray-500 text-xs">
-          &copy; {new Date().getUTCFullYear()} StoreHive Corp. All rights
-          reserved.
-        </p>
+        <div className="divider">otherwise</div>
+        <div className="flex justify-center gap-2">
+          <Button varient="outlined">
+            <FaGoogle />
+          </Button>
+          <Button varient="outlined">
+            <FaTwitter />
+          </Button>
+          <Button varient="outlined">
+            <FaGithub />
+          </Button>
+        </div>
       </div>
     </div>
   );
--- src/app/(auth)/signin/page.tsx
+++ src/app/(auth)/signin/page.tsx
@@ -6,9 +6,9 @@ const SigninPage: NextPage = () => {
     <main className="h-[calc(100vh-10rem)] flex flex-col justify-center">
       <div className="text-center mb-2">
         <h1 className="text-brand-secondary font-semibold text-2xl ">
-          Join the hive!
+          Login into the hive!
         </h1>
-        <h2>and discover endless shopping possibilities</h2>
+        <h2>Buzzing with great deals and offers</h2>
       </div>
       <AuthForm />
     </main>
