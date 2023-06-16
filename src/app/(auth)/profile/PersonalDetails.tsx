3de36e31e5a7c27765d83bd02b25ed838ba2a813
Bineet Naidu <bineet.s.naidu@gmail.com>
2023-06-16 16:23:55 +0530

    ☘️ handle Personal Detail updates

--- src/app/(auth)/profile/PersonalDetails.tsx
+++ src/app/(auth)/profile/PersonalDetails.tsx
@@ -1,7 +1,15 @@
-import { FC } from 'react';
+'use client';
+
+import { FC, useState, useCallback } from 'react';
 import { BsCheck2Circle } from 'react-icons/bs';
 import { FaEdit, FaGoogle, FaTwitter, FaGithub } from 'react-icons/fa';
+import { RxCross2 } from 'react-icons/rx';
 import { Button } from '@/components/Button';
+import { InputField } from '@/components/InputField';
+import Image from 'next/image';
+import { UpdateProfileResponse } from '@/app/api/auth/profile/route';
+import { toast } from 'react-toastify';
+import { useSession } from 'next-auth/react';
 
 type Props = {
   profile: {
@@ -18,22 +26,133 @@ type Props = {
 };
 
 export const PersonalDetails: FC<Props> = ({ profile }) => {
+  const [isEditingMode, setIsEditingMode] = useState(false);
+  const [isUpdating, setIsUpdating] = useState(false);
+  const [name, setName] = useState(profile.name!);
+  const [image, setImage] = useState(profile.image!);
+  const [phoneNumber, setPhoneNumber] = useState('+100 9876543210');
+  const [error, setError] = useState({
+    name: '',
+    image: '',
+    phoneNumber: '',
+  });
+
+  const { update } = useSession();
+
+  const handleUpdate = useCallback(async () => {
+    if (!name) {
+      setError((prev) => ({ ...prev, name: 'Name is required.' }));
+      return;
+    }
+
+    if (!image) {
+      setError((prev) => ({ ...prev, image: 'Image is required.' }));
+      return;
+    }
+
+    if (!phoneNumber) {
+      setError((prev) => ({
+        ...prev,
+        phoneNumber: 'Phone number is required.',
+      }));
+      return;
+    }
+
+    setIsUpdating(true);
+
+    try {
+      const res = await fetch('/api/auth/profile', {
+        method: 'PUT',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          name,
+          image,
+          phoneNumber,
+        }),
+      });
+
+      const data: UpdateProfileResponse = await res.json();
+
+      if (data.errors) {
+        setError({
+          name: data.errors.name?.join(', ') || '',
+          image: data.errors.image?.join(', ') || '',
+          phoneNumber: data.errors.phoneNumber?.join(', ') || '',
+        });
+
+        setIsUpdating(false);
+        return;
+      }
+
+      if (!data.result) {
+        toast.error('Something went wrong!');
+        setIsUpdating(false);
+        return;
+      }
+
+      await update({
+        name: data.result.name,
+        image: data.result.image,
+      });
+
+      toast.success('Profile updated successfully!', {
+        icon: <BsCheck2Circle size={15} />,
+      });
+
+      setIsEditingMode(false);
+      setIsUpdating(false);
+    } catch (error: any) {
+      toast.error(error.message);
+      setIsUpdating(false);
+    }
+  }, [name, image, phoneNumber, update]);
+
   return (
-    <div className="mt-4 border border-gray-200 rounded-md p-4 w-[500px] shadow-sm bg-gray-100">
+    <div className="mt-4 border border-gray-200 rounded-md p-4 w-full md:w-[500px] shadow-sm bg-gray-100">
       <div className="flex justify-between">
         <h1 className="text-2xl font-bold text-brand-font-color">
           Personal Details
         </h1>
 
-        <Button size="sm">
-          <FaEdit size={16} />
-        </Button>
+        <div className="flex gap-1">
+          {isEditingMode && (
+            <Button
+              type="button"
+              size="sm"
+              onClick={handleUpdate}
+              isLoading={isUpdating}
+            >
+              Update!
+            </Button>
+          )}
+
+          <Button
+            type="button"
+            size="sm"
+            onClick={() => setIsEditingMode((prev) => !prev)}
+            disabled={isUpdating}
+          >
+            {isEditingMode ? <RxCross2 size={15} /> : <FaEdit size={15} />}
+          </Button>
+        </div>
       </div>
 
-      <div className="grid grid-cols-2 gap-4 mt-4 w-96">
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-4 mt-4 w-96">
         <div className="flex flex-col space-y-1">
           <h2 className="text-sm">Name</h2>
-          <p className="text-gray-700 font-medium">{profile.name}</p>
+          {isEditingMode ? (
+            <InputField
+              type="text"
+              value={name}
+              onChange={(e) => setName(e.target.value)}
+              message={error.name}
+              placeholder="John Doe"
+            />
+          ) : (
+            <p className="text-gray-700 font-medium">{profile.name}</p>
+          )}
         </div>
 
         <div className="flex flex-col space-y-1">
@@ -43,7 +162,38 @@ export const PersonalDetails: FC<Props> = ({ profile }) => {
 
         <div className="flex flex-col space-y-1">
           <h2 className="text-sm">Phone</h2>
-          <p className="text-gray-700 font-medium">+100 9876543210</p>
+          {isEditingMode ? (
+            <InputField
+              type="text"
+              value={phoneNumber}
+              onChange={(e) => setPhoneNumber(e.target.value)}
+              message={error.phoneNumber}
+              placeholder="+100 9876543210"
+            />
+          ) : (
+            <p className="text-gray-700 font-medium">{phoneNumber}</p>
+          )}
+        </div>
+
+        <div className="flex flex-col space-y-1">
+          <h2 className="text-sm">Image</h2>
+          {isEditingMode ? (
+            <InputField
+              placeholder="https://example.com/image.png"
+              type="url"
+              value={image}
+              onChange={(e) => setImage(e.target.value)}
+              message={error.image}
+            />
+          ) : (
+            <Image
+              src={profile.image!}
+              alt="Profile Image"
+              width={30}
+              height={30}
+              className="rounded-full"
+            />
+          )}
         </div>
 
         <div className="flex flex-col space-y-1">
--- src/app/api/auth/profile/route.ts
+++ src/app/api/auth/profile/route.ts
@@ -1,6 +1,86 @@
 import { prisma } from '@/lib/prisma';
 import { getServerSession } from 'next-auth';
 import { NextResponse } from 'next/server';
+import { z } from 'zod';
+
+export async function PUT(req: Request) {
+  const session = await getServerSession();
+
+  if (!session) {
+    return NextResponse.json(
+      {
+        error: 'Unauthorized',
+      },
+      { status: 401 }
+    );
+  }
+
+  const updateReqBodySchema = z.object({
+    name: z
+      .string()
+      .min(3, { message: 'Name must be at least 3 characters long' })
+      .max(255, { message: 'Name must be at most 255 characters long' }),
+    image: z.string().url({ message: 'Image must be a valid URL' }),
+    phoneNumber: z
+      .string()
+      .min(10, { message: 'Phone number must be at least 10 characters long' })
+      .max(10, { message: 'Phone number must be at most 10 characters long' }),
+  });
+
+  const body = await req.json();
+
+  const parsedBody = updateReqBodySchema.safeParse(body);
+
+  if (!parsedBody.success) {
+    return NextResponse.json(
+      {
+        errors: parsedBody.error.flatten().fieldErrors,
+      },
+      {
+        status: 400,
+      }
+    );
+  }
+
+  const user = await prisma.user.update({
+    where: {
+      email: session.user.email,
+    },
+    data: {
+      name: parsedBody.data.name,
+      image: parsedBody.data.image,
+    },
+    select: {
+      id: true,
+      email: true,
+      name: true,
+      image: true,
+    },
+  });
+
+  return NextResponse.json(
+    {
+      result: user,
+    },
+    {
+      status: 200,
+    }
+  );
+}
+
+export type UpdateProfileResponse = {
+  result?: {
+    id: string;
+    email: string;
+    name: string | null;
+    image: string | null;
+  };
+  errors?: {
+    name?: string[] | undefined;
+    image?: string[] | undefined;
+    phoneNumber?: string[] | undefined;
+  };
+};
 
 export type DeleteProfileResponse = {
   error?: string;
